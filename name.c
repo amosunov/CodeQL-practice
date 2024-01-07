2b99adb2ede695e1f31af3df822bca11744808f3
Anton Mosunov <mosunoff@gmail.com>
2024-01-07 17:12:54 -0500

    Update name.c

--- name.c
+++ name.c
@@ -5,6 +5,9 @@ int main() {
     char buffer[10];
     printf("Enter an integer representing degrees in Celcius: ");
     fgets(buffer, 10, stdin); 
-    int c = atoi(buffer); 
-    printf("%d degrees Celcius approximates to %d degrees Fahrenheit.\n", c, (int) (1.8*((double) c) + 32.0)); 
+    int c = atoi(buffer);
+    fgets(buffer, 10, stdin);
+    int d = atoi(buffer);
+    //printf("%d degrees Celcius approximates to %d degrees Fahrenheit.\n", c, (int) (1.8*((double) c) + 32.0)); 
+    printf("%d\n", c * d);
 } 
