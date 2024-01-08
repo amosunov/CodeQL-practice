3f1fbba550cca958407f1427da315d5810de1d1d
Anton Mosunov <mosunoff@gmail.com>
2024-01-07 19:11:38 -0500

    Update name.c

--- name.c
+++ name.c
@@ -3,8 +3,16 @@
 
 int main(int argc, char** argv) {
     FILE* fp = fopen(argv[1], "r");
+
+    if (fp == NULL) {
+        printf("Unable to open the file.\n");
+        exit(1);
+    }   
+
     fseek(fp, 0L, SEEK_END);
     int sz = ftell(fp);
     printf("%d\n", sz);
     fclose(fp);
+
+    return 0;
 }
