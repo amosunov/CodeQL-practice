2280b7bcc081269b55af0788c9474d9f5d897bf8
Anton Mosunov <mosunoff@gmail.com>
2024-02-24 08:44:43 -0500

    Update size.c

--- size.c
+++ size.c
@@ -1,11 +1,22 @@
-#include <stdlib.h>
-#include <string.h>
-
-#define BUFSIZE 256
+#include <stdio.h>
 
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
-    system(cmd);
+    // opening the file in read mode 
+    FILE* fp = fopen(argv[1], "r"); 
+  
+    // checking if the file exist or not 
+    if (fp == NULL) { 
+        printf("File not found\n"); 
+        return -1; 
+    }   
+  
+    fseek(fp, 0L, SEEK_END); 
+  
+    // calculating the size of the file 
+    long size = ftell(fp); 
+    printf("%ld\n", size); 
+ 
+    // closing the file 
+    fclose(fp); 
 }
