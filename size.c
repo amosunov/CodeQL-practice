e193590d21c32f9f784dcd4a8e36f6824cb8fa8c
Anton Mosunov <mosunoff@gmail.com>
2024-02-24 08:52:26 -0500

    Update size.c

--- size.c
+++ size.c
@@ -1,22 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// This program prints the size of a specified file in bytes
-int main(int argc, char** argv) {
-    // opening the file in read mode 
-    FILE* fp = fopen(argv[1], "r"); 
-  
-    // checking if the file exist or not 
-    if (fp == NULL) { 
-        printf("File not found\n"); 
-        return -1; 
-    }   
-  
-    fseek(fp, 0L, SEEK_END); 
-  
-    // calculating the size of the file 
-    long size = ftell(fp); 
-    printf("%ld\n", size); 
- 
-    // closing the file 
-    fclose(fp); 
+int main(int argc, char **argv) {
+    char *items[] = {"boat", "car", "truck", "train"};
+    int index = atoi(argv[1]);
+    printf("You selected %s\n", items[index-1]);
 }
