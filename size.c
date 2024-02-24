b7621758989e1654b17f2f42b7c3ec8e478b1120
Anton Mosunov <mosunoff@gmail.com>
2024-02-24 08:25:45 -0500

    Update size.c
    Added a comment

--- size.c
+++ size.c
@@ -3,6 +3,7 @@
 
 #define BUFSIZE 256
 
+// This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
     char cmd[BUFSIZE] = "wc -c < ";
     strcat(cmd, argv[1]);
