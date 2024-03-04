ccbf8dd0878e15e6bee15ea925a1327cfe842f56
Anton Mosunov <mosunoff@gmail.com>
2024-03-04 10:57:44 -0500

    Update name.c

--- name.c
+++ name.c
@@ -1,8 +1,11 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define BUFSIZE 256
+
+// This program prints the size of a specified file in bytes
 int main(int argc, char **argv) {
-    char *items[] = {"boat", "car", "truck", "train"};
-    int index = atoi(argv[1]);
-    printf("You selected %s\n", items[index-1]);
+    char cmd[BUFSIZE] = "wc -c , ";
+    strcat(cmd, argv[1]);
+    system(cmd);
 }
