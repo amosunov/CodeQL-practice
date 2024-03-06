a168c618acc3441c31e1c01b607eb091e47e488d
Anton Mosunov <mosunoff@gmail.com>
2024-03-06 11:16:00 -0500

    Update filesize.c

--- filesize.c
+++ filesize.c
@@ -5,7 +5,7 @@
 
 // This program prints the size of a specified file in bytes
 int main(int argc, char **argv) {
-    char cmd[BUFSIZE] = "wc -c , ";
+    char cmd[BUFSIZE] = "wc -c ";
     strcat(cmd, argv[1]);
     system(cmd);
 }
