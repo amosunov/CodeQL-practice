a1e414e5258d26e103cce1a83d57b5660ca9e130
Anton Mosunov <mosunoff@gmail.com>
2024-06-12 19:14:25 -0400

    Update filesize.c

--- filesize.c
+++ filesize.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int main(int argc, char** argv) {
-  char *userName = argv[1];
-  
-    char command1[1000] = {0};
-    sprintf(command1, "ls %s", userName);
-    system(command1);
+int main() {
+    char last_name[20];
+    printf("Enter your last name: ");
+    scanf("%s", last_name);
+    printf("Entered last name is %s\n", last_name);
 }
 /*
 #include <stdlib.h>
