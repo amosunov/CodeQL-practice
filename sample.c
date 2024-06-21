2f8697f8919b4fc61069dfc1eb079ef6843882c8
Anton Mosunov <mosunoff@gmail.com>
2024-06-21 15:23:15 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,45 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <unistd.h>   // For access() function
+#include <sys/stat.h> // For stat() function
 
 #define BUFSIZE 256
 
-#define ALLOWED_CHARS "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/_-. <>"
+// Function to encode a shell command argument
+void encode_shell_string(const char *input, char *output, size_t out_size) {
+    size_t len = strlen(input);
+    size_t pos = 0;
 
-void
-sanitize_path(char  *path) {
-    char *p = path;
+    for (size_t i = 0; i < len && pos < out_size - 1; ++i) {
+        if (input[i] == '"' || input[i] == '\\' || input[i] == '$') {
+            output[pos++] = '\\'; // Escape special characters
+        }
+        output[pos++] = input[i];
+    }
 
-    while(*p) {
-        if (!strchr(ALLOWED_CHARS, *p)) {
-            *p = '_'; // replace disallowed characters with underscore
-    }
-    p++;
-    }
+    output[pos] = '\0'; // Null-terminate the output string
 }
 
-int main(int argc, char ** argv) {
-    if  (argc != 2) {
+// Function to check if a file exists
+int file_exists(const char *filename) {
+    struct stat buffer;
+    return (stat(filename, &buffer) == 0);
+}
+
+// Function to print file size in bytes
+void print_file_size(const char *filename) {
+    char cmd[BUFSIZE];
+    char encoded_filename[BUFSIZE];
+
+    encode_shell_string(filename, encoded_filename, sizeof(encoded_filename));
+
+//for executing in UNIX
+    snprintf(cmd, sizeof(cmd), "wc -c < \"%s\"", encoded_filename);
+// for executing in Windows CMD 
+// snprintf(cmd, sizeof(cmd), "type %s | find /v /c \"\"", encoded_filename);
+
+    system(cmd); // Execute the command
+}
+
+int main(int argc, char** argv) {
+    // Ensure that the user supplied exactly one command line argument
+    if (argc != 2) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-
-    char sanitized_path[BUFSIZE];
-
-    strncpy(sanitized_path, argv[1], BUFSIZE - 1);
-
-    sanitized_path[BUFSIZE - 1] = '\0';
-
-    struct stat st;
-
-    if (stat(sanitized_path, &st) < 0) {
-        perror("stat");
+   
+    // Check if the file exists and is accessible
+    if (!file_exists(argv[1])) {
+        fprintf(stderr, "File %s does not exist or cannot be accessed.\n", argv[1]);
         return -1;
     }
 
-    printf("%ld\n", (long) st.st_size);
+    // Print the size of the specified file
+    print_file_size(argv[1]);
 
     return 0;
 }
