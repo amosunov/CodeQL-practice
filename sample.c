ba7052aa6ff52ce2c6154a9018622549a111e3f5
Anton Mosunov <mosunoff@gmail.com>
2024-07-09 12:00:03 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,31 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+                                
 #define BUFSIZE 256
-
+                                
+// This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
-    if (argc != 2) {
-        fprintf(stderr, "Error\n");
-        return -1; 
-    }   
-
-    char* filename = argv[1];
-
-    FILE* fp = fopen(filename, "r");
- 
-    if (fp == NULL) {
-        fprintf(stderr, "Error\n");
-        return -1; 
-    }   
-
-    if (fclose(fp) != 0) {
-        fprintf(stderr, "Error\n");
-        return -1; 
-    }   
-
+    // Ensure that the user supplied exactly one command line argument
+    if (argc != 2) { 
+      fprintf(stderr, "Please provide the address of a file as an input.\n");
+      return -1;
+    }
     char cmd[BUFSIZE] = "wc -c < ";
-    strncat(cmd, filename, BUFSIZE);
+    strcat(cmd, argv[1]);
     system(cmd);
-    return 0;
 }
